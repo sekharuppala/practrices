de0f469e0f8fbb53c02c5aef81f077974e3d014a
Sekhar gowd <sekhargowd645@gmail.com>
2026-02-17 14:19:12 +0530

    Learning

---
+++ src/Variables.java
@@ -0,0 +1,95 @@
+/*public class Variables {
+    public static void main(String... args) {
+        System.out.println("hello world");
+
+//        int a = 10;
+         double b = 20.52;
+        float c = 30.5455f;
+        String d = "sekhar";
+        Long e = 12423L;
+        short f= 21554;
+
+//        System.out.println(a-e);
+        System.out.println("my name is sekhar");
+        System.out.println(c+d);
+
+
+    }
+}*/
+
+
+//public class Variables {
+//    public static void main(String ... args){
+//        while(true){
+//
+//            if(user)
+
+
+
+
+//
+//                class student{
+//                    //declaring fields or instance variables
+//                    int id;
+//                    String name;
+//                }
+////Creating another class which contains the main() method
+//            class Main{
+//                public static void main(String ... args){
+//                    student s1=new student();
+//                    System.out.println(s1.id);
+//                    System.out.println(s1.name);
+//                }
+//            }
+
+
+//
+//class Calculator {
+//
+//    int add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    int subtract(int a, int b) {
+//        return a - b;
+//    }
+//}
+//
+//public class Variables {
+//    public static void main(String[] args) {
+//        Calculator calc = new Calculator();
+//
+//        System.out.println("Addition: " + calc.add(10, 5));
+//        System.out.println("Subtraction: " + calc.subtract(10, 5));
+//    }
+//}
+//
+//public class Variables {
+//    // Method definition
+//    public int add(int a, int b) {
+//        int sum = a + b; // Method body
+//        return sum; // Return statement
+//    }
+//}
+
+
+
+class Variables{
+    public static void main() {
+        System.out.println("Hello world");
+        System.out.println("Good Morning");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
